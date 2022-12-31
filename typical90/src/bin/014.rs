e880cbba133bf89e9fc827e5c6709317080697d0
cohsh <82075327+cohsh@users.noreply.github.com>
2022-12-31 11:41:04 +0900

    [update] 014: solved with O(N logN)

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -2,5 +2,16 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{n: usize, mut a: [usize; n], mut b: [usize; n]}
+
+    a.sort_unstable();
+    b.sort_unstable();
+
+    let mut answer = 0;
+
+    for i in 0..n {
+        answer += (a[i] as isize - b[i] as isize).abs();
+    }
+
+    println!("{}", answer);
 }
