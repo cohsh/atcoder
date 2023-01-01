84aadbd4d223111563c017964c72babc5bdeea1c
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-01 18:59:29 +0900

    [update] 016: solved with O(M^2)

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -1,6 +1,22 @@
 use proconio::{input, fastout};
+use std::cmp;
+
+const M: isize = 9999;
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{n: isize, a: isize, b: isize, c: isize}
+    let mut answer = 1 << 30;
+    for i in 0..=M {
+        for j in 0..=M-i {
+            let v = n - a * i - b * j;
+            if v % c == 0 {
+                let k = v / c;
+                if k >= 0 && k <= M {
+                    answer = cmp::min(answer, i + j + k);
+                }
+            }
+        }
+    }
+    println!("{}", answer);
 }
