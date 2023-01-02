a11fde2b1f28e86d5c7e5fcf8c5593e03b373d1b
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-02 16:39:51 +0900

    [update] 048: solved with O(N logN)

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -2,5 +2,22 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{
+        n: usize, k: usize,
+        c: [[isize; 2]; n],
+    }
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(c[i][1]);
+        vec.push(c[i][0] - c[i][1]);
+    }
+
+    let mut answer = 0;
+    vec.sort();
+    vec.reverse();
+    for i in 0..k {
+        answer += vec[i];
+    }
+
+    println!("{}", answer);
 }
