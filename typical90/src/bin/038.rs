b1ae4d5ffcbeafee55f885813b27fbc3cdabc20e
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-02 13:43:55 +0900

    [update] 038: solved with O(log(A+B))

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -2,5 +2,17 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{a: u128, b: u128}
+
+    let answer = a * (b / gcd(a, b));
+    if answer <= 10_u128.pow(18) {
+        println!("{}", answer)
+    } else {
+        println!("Large")
+    }
+}
+
+fn gcd(a: u128, b: u128) -> u128 {
+    if b == 0 {return a}
+    return gcd(b, a % b);
 }
