54fcbbe67076613b3c7e2b6a7eb7ee74f8cd911d
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-02 10:58:33 +0900

    [update] 020: solved with O(1)

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -2,5 +2,11 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{a: u64, b: u32, c: u64}
+
+    if a < c.pow(b) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
