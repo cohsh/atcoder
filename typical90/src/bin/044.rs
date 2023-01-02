62a53071e0752ba244d8824328e770cb2b072e64
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-02 14:33:26 +0900

    [update] 044: solved with O(N+Q)

--- typical90/src/bin/044.rs
+++ typical90/src/bin/044.rs
@@ -2,5 +2,22 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{
+        n: usize, q: usize,
+        mut a: [usize; n],
+        b: [[isize; 3]; q],
+    }
+    
+    let mut shift = 0;
+    for i in 0..q {
+        let t = b[i][0];
+        let x = (b[i][1] - 1 + shift) % n as isize;
+        let y = (b[i][2] - 1 + shift) % n as isize;
+        match t {
+            1 => a.swap(x as usize, y as usize),
+            2 => shift = (shift + n as isize - 1) % n as isize,
+            3 => println!("{}", a[x as usize]),
+            _ => (),
+        }
+    }
 }
