73b049b831c03aa77dfefa4609ec4b8a429a04cc
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-02 16:56:57 +0900

    [update] 050: solved with O(N)

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -2,5 +2,14 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{n: usize, l: usize};
+
+    let div: usize = 10_usize.pow(9) + 7;
+    let mut dp = vec![1; n+1];
+    for i in 1..=n {
+        if i < l {dp[i] = dp[i-1]}
+        else {dp[i] = (dp[i-1] + dp[i-l]) % div};
+    }
+
+    println!("{}", dp[n]);
 }
