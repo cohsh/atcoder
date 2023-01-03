ff6e8609b531e58df0229edd6eeff74a2bb09d8a
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-03 16:50:11 +0900

    [update] 069: solved with O(logN)

--- typical90/src/bin/069.rs
+++ typical90/src/bin/069.rs
@@ -1,6 +1,37 @@
 use proconio::{input, fastout};
 
+const DIV: usize = 1000000007;
+
 #[fastout]
 fn main() {
-    todo!();
+    input!{n: usize, k: usize}
+
+    let answer: usize;
+    if k == 1 {
+        if n == 1 {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+    } else if n == 1 {
+        answer = k % DIV;
+    } else if n == 2 {
+        answer = k * (k - 1) % DIV;
+    } else {
+        answer = k * (k - 1) % DIV * pow(k - 2, n - 2) % DIV;
+    }
+
+    println!("{}", answer);
+}
+
+fn pow(mut a: usize, mut b: usize) -> usize {
+    let mut answer = 1;
+    while b != 0 {
+        if b % 2 == 1 {
+            answer = answer * a % DIV;
+        }
+        a = a * a % DIV;
+        b /= 2;
+    }
+    return answer;
 }
