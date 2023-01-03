0961c820658cd474942441e2f5fba73026c3d1e6
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-03 11:48:16 +0900

    [update] 052: solved with O(N)

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -2,5 +2,21 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    let div = 10_usize.pow(9) + 7;
+    input!{
+        n: usize, a: [[usize; 6]; n],
+    }
+
+    let mut answer = 1;
+    let mut tmp: usize;
+    for i in 0..n {
+        tmp = 0;
+        for j in 0..6 {
+            tmp += a[i][j];
+        }
+        answer *= tmp;
+        answer %= div;
+    }
+
+    println!("{}", answer);
 }
