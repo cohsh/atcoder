46140ffeaa0ae61b3e94213546a668a66ccb19c4
cohsh <82075327+cohsh@users.noreply.github.com>
2023-01-05 12:10:59 +0900

    [update] 075: solved with O(\sqrt{N})

--- typical90/src/bin/075.rs
+++ typical90/src/bin/075.rs
@@ -2,5 +2,39 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input!{tmp: usize}
+    let n = Int {value: tmp};
+    let vec = n.prime_factorization();
+
+    let mut answer = 1;
+    for i in 0..=20 {
+        if (1 << i) >= vec.len() {
+            answer = i;
+            break;
+        }
+    }
+    println!("{}", answer);
+}
+
+pub struct Int {
+    value: usize,
+}
+
+impl Int {
+    pub fn prime_factorization(&self) -> Vec<usize> {
+        let mut rem = self.value;
+        let mut p = Vec::new();
+        let mut i = 2;
+        while i * i <= self.value {
+            while rem % i == 0 {
+                rem /= i;
+                p.push(i);
+            }
+            i += 1;
+        }
+        if rem != 1 {
+            p.push(rem);
+        }
+        return p;
+    }
 }
